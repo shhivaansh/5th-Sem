844033f34585d82c5f2a8ee07b3960e57a6cdf6c
shhivaansh <166250589+shhivaansh@users.noreply.github.com>
2024-12-12 21:59:59 +0530

    E7_gauss_jordan_method.c

--- CBNST/E7_gauss_jordan_method.c
+++ CBNST/E7_gauss_jordan_method.c
@@ -3,7 +3,7 @@
 
 void details()
 {
-	printf("Name: Divyansh Dhaundiyal\nSection: D(G1)\nRno: 31\n\nGauss Jordan Method\n\n");
+	printf("Name: Shivansh Gaur\nSection: D(G2)\nRno: 63\n\nGauss Jordan Method\n\n");
 }
 
 void main()
