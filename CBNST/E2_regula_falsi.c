945f956f542b83182a59242f1f2d54c29c9d53d4
shhivaansh <166250589+shhivaansh@users.noreply.github.com>
2024-12-12 21:57:58 +0530

    E2_regula_falsi.c

--- CBNST/E2_regula_falsi.c
+++ CBNST/E2_regula_falsi.c
@@ -3,7 +3,7 @@
 
 void details()
 {
-    printf("Name: Divyansh Dhaundiyal\nSection: D(G1)\nRno: 31\n\nRegula Falsi Method\n\n");
+    printf("Name: Shivansh Gaur\nSection: D(G2)\nRno: 63\n\nRegula Falsi Method\n\n");
 }
 
 float f(float x)
