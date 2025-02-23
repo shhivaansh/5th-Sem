b59ee6fef3ec78e643b9d7fef9e136d15fe3c057
shhivaansh <166250589+shhivaansh@users.noreply.github.com>
2025-02-23 22:53:37 +0530

    E13_shared_memory.c

---
+++ OS/E13_shared_memory.c
@@ -0,0 +1,40 @@
+//reader
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+int main()
+{
+  key_t key;
+  int shmid;
+  void *ptr;
+  key=ftok("srfile",'A');
+  shmid=shmget(key,1024,0666|IPC_CREAT);
+  ptr=shmat(shmid,(void *)0,0);
+  printf("\nThe data stored is: %s\n",ptr);
+  shmdt(ptr);
+  shmctl(shmid,IPC_RMID,NULL);
+  return 0;
+}
+
+//writer
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+int main()
+{
+  key_t key;
+  int shmid;
+  void *ptr;
+  key=ftok("shmfile",'A');
+  shmid=shmget(key,1024,0666|IPC_CREAT);
+  ptr=shmat(shmid,(void *)0,0);
+  printf("\nInput Data: ");
+  gets(ptr);
+  shmdt(ptr);
+  return 0;
+}
